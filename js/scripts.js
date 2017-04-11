0a32c2098afc6297fd522bab88937e5d97e79818
cashan <cashan@wp.pl>
2017-04-11 20:27:44 +0200

    ex1-ready

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var url = 'http://api.icndb.com/jokes/random';
+getJoke();
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function () {
+	getJoke();
+});
+
+function getJoke() {
+	var paragraph = document.getElementById('joke');
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function () {
+		var response = JSON.parse(xhr.response);
+		paragraph.innerHTML = response.value.joke;
+	});
+	xhr.send();
+}
