d81c022495ea35eec51792d2a2fcecc06fb9af00
cashan <cashan@wp.pl>
2017-04-14 09:19:22 +0200

    ex3 ready

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(function () {
+
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
+
+	$('#search').on('click', searchCountries);
+
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+		if (!countryName)
+			countryName = 'Poland';
+		//
+		$.ajax({
+			url: url + countryName,
+			method: 'GET',
+			success: showCountriesList
+		});
+	}
+
+	function showCountriesList(resp) {
+		countriesList.empty();
+		resp.forEach(function (item) {
+			$('<li>').text(item.name + " - " + item.capital).appendTo(countriesList);
+		})
+	}
+});
